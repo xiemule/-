9f848849da3673d0eae15baf13f24ddbb8cc3085
xiemule <144131319+xiemule@users.noreply.github.com>
2023-11-05 11:43:15 +0800

    Add files via upload

---
+++ Jiafa.java
@@ -0,0 +1,11 @@
+public class Jiafa {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 6;
+        int sum = add (num1,num2);
+        System.out.println("和为: "+sum);
+    }
+    public static int add(int num1,int num2){
+        return num1 + num2;
+    }
+}
